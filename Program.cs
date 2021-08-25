2d89c82b987969383e0c8ca8063655f88d54e8c2
warrior14 <ironaddicts14@gmail.com>
2021-08-25 15:30:55 -0500

    added a guess limit

--- Program.cs
+++ Program.cs
@@ -32,8 +32,60 @@ namespace GuessingGame
 
             Console.Write("What is your guess?");
             string guess = Console.ReadLine();
-            int parsedGuess = int.Parse(guess);
-            int secretNum = 21;
+            int parsedGuess = int.Parse(guess); // converting string to int
+            Random randomNum = new Random(); // instantiating new Random number object from static method
+            int secretNum = randomNum.Next(1, 100); // storing random number between 1 and 100 into secretNum variable
+            int guessCount = 0; // initial guess count
+            int guessLimit = 3; // guess limit of 4 chances to guess secret number since initial count is 0
+            bool outOfGuesses = false; // boolean indicating if user is out of guesses
+
+
+          // while user's guess does not match secret number and user is not out of guesses, keep looping/asking the user to guess
+          while(parsedGuess != secretNum && !outOfGuesses)
+          {
+            if (guessCount < guessLimit) // if user has not reached guess limit, execute code below:
+            {
+              if (parsedGuess > secretNum)
+              {
+                Console.WriteLine(@"
+                
+                
+                
+                
+              .-----.
+            .' -   - '.
+            /  .-. .-.  \
+            |  | | | |  |
+            \  \o/ \o/  /           Your guess is incorrect!
+            _/    ^    \_           Your guess was too high.
+            | \  '---'  / |         Please try again!
+            / /`--. .--`\ \
+            / /'---` `---'\ \  
+            '.__.       .__.'
+                `|     |`
+                 |     \
+                 \      '--.
+                  '.        `\
+                    `'---.   |
+                       ,__) /
+                        `..'
+
+                
+                
+                
+                ");
+
+              
+              int guessesLeft = guessLimit - guessCount; // variable will store number of guesses user has left and displays count:
+
+
+
+
+              }
+            }
+          }
+
+
 
             if (parsedGuess == secretNum)
             {
